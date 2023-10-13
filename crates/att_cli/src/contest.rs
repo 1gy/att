2842e4e907e44fdb4136f65f9be978d5411ec9af
1gy <42576495+1gy@users.noreply.github.com>
2023-10-13 14:01:06 +0000

    コンテスト設定を追加

---
+++ crates/att_cli/src/contest.rs
@@ -0,0 +1,43 @@
+use std::path::PathBuf;
+
+use crate::search_file;
+
+const CONSTEST_FILE_NAME: &str = ".atcontest.json";
+
+#[derive(Debug, serde::Deserialize)]
+pub struct ContestInfo {
+    pub id: String,
+    pub title: String,
+    pub url: String,
+}
+
+#[derive(Debug, serde::Deserialize)]
+pub struct ContestConfig {
+    pub contest: ContestInfo,
+}
+
+#[derive(Debug)]
+pub struct Contest {
+    pub directory_path: PathBuf,
+    pub contest: ContestConfig,
+}
+
+pub fn load_contest() -> Option<Contest> {
+    let working_directory = match std::env::current_dir() {
+        Ok(path) => path,
+        Err(_) => PathBuf::new(),
+    };
+    if let Some(searct_result) = search_file(working_directory, CONSTEST_FILE_NAME) {
+        if let Ok(file) = std::fs::OpenOptions::new()
+            .read(true)
+            .open(searct_result.file_path)
+        {
+            let contest: ContestConfig = serde_json::from_reader(file).unwrap();
+            return Some(Contest {
+                directory_path: searct_result.directory_path,
+                contest,
+            });
+        }
+    }
+    None
+}
